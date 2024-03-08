04f95580faa3a80d9a606f2410ff846355825420
Ranvijay Singh <ranvijsysingh2012@gmail.com>
2024-03-08 20:44:51 +0530

    Initial commit

---
+++ src/test/java/com/ranvijay2012/springBoot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranvijay2012.springBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
